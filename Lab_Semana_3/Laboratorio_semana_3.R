1d20d554a04aa26d32e3a30eb68be8c2a2cee247
toryferrer <toryferrer@live.com>
2023-02-22 17:58:05 -0600

    parte_4

--- Lab_Semana_3/Laboratorio_semana_3.R
+++ Lab_Semana_3/Laboratorio_semana_3.R
@@ -11,17 +11,19 @@ head(trees)
 
 dbh <- c(16.5, 25.3,22.1, 17.2, 16.1, 8.1, 34.3, 5.4, 5.7, 11.2, 24.1, 14.5, 7.7, 15.6, 15.9, 10.0, 17.5, 20.5, 7.8, 27.3, 9.7, 6.5, 23.4, 8.2, 28.5, 10.4, 11.5, 14.3, 17.2, 16.8)
 
+#accesar a datos de internet
+
 #datos de URL no seguras (http)
 
-prof_url <- source_data("http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv")
-head(prof_url)
+#estos datos se ingresaron de la siguiente manera dado que no se supo la libreria para que corriera como venia en el ejercicio
 
-library(reader)
-prof_ur2 <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv"
-profepa <- read.csv(prof_ur2)
+#ademas se agrego el # para ponerlos como texto porque al momento de copilarlos no dejaba y marcaba como que el programa no econtro el archivo especificado
 
+#prof_url <- source_data("http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionfoanp.csv")
+#head(prof_url)
 
 #datos de URL seguras (https)
+
 library(repmis)
 conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
 head(conjunto)
@@ -64,6 +66,8 @@ which(trees$dbh<10)
 
 trees.13 <- trees[!(trees$parcela=="2")]
 
+head(trees.13)
+
 #seleccion de una submuestra #diametro menores o iguales a 10
 
 trees.1 <- subset(trees, dbh<=10)
@@ -72,6 +76,11 @@ head(trees.1)
 mean(trees$dbh)
 
 mean(trees.1$dbh)
+
+hist(trees$dbh, ylab = "Frecuencia", xlab = "DBH", main = "Muestra orinal trees")
+
+hist(trees.1$dbh, ylab = "Frecuencia", xlab = "DBH",  main = "dbh < 10 cm. trees.1")
+
 
 # Parte 3 Representacion grafica ------------------------------------------
 
@@ -84,6 +93,8 @@ head(mamiferos)
 #Por el momento trabajaremos con la variable total_sleep para generar el histograma:
 
 hist(mamiferos$total_sleep)
+
+# dar formato a las histogramas 
 
 hist(mamiferos$total_sleep, #datos
      xlim = c(0,20), ylim = c(0,14), main = "Total de horas sueño de las 39 especies", #cambiar titulo
@@ -99,13 +110,19 @@ data("chickwts")
 
 head(chickwts[c(1:2, 42:43, 62:64),])
 
+# acomodar datos en columnas 
+
 feeds <- table(chickwts$feed)
 feeds
 
 barplot(feeds)
 
-barplot(feeds[order(feeds, decreasing = TRUE)], main = "Frecuencias por tipos de
+# ordenarla de forma decreciente y darle presentacion a la grafica 
+barplot(feeds[order(feeds, decreasing = FALSE)], main = "Frecuencias por tipos de
 alimentación",
-        ylab = "numero de pollos")
+        xlab = "Numero de pollos",
+        las=1,
+        col= "yellow",
+        horiz = TRUE)
 
 
